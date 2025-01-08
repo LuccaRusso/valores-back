36c5b9141f5fd47c8f9e3facd1f6f1a6073883d2
G54525 <luccapererusoo@gmail.com>
2025-01-08 11:28:14 -0300

    Iniciando projeto no GitHub

---
+++ src/main/java/com/example/CardGame/controller/CardGameController.java
@@ -0,0 +1,41 @@
+package com.example.CardGame.controller;
+
+import com.example.CardGame.database.Connection;
+import com.example.CardGame.dto.User;
+import com.example.CardGame.service.CardGameService;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import lombok.RequiredArgsConstructor;
+import org.bson.conversions.Bson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://127.0.0.1:5500")
+@RestController
+@RequiredArgsConstructor
+public class CardGameController {
+
+    @Autowired
+    private CardGameService cardGameService;
+
+    @PostMapping("/cadastro")
+    public Boolean doPost(@RequestBody User user) {
+        return CardGameService.verify(user);
+    }
+
+    @PostMapping("/login")
+    public Boolean doLogin(@RequestBody User user){
+        return CardGameService.findLogin(user);
+    }
+
+    @PostMapping("/pontuar")
+    public String doFilter(@RequestBody User user){
+        CardGameService.insertPoint(user.getEmail());
+        return "Sua pontuação " + user.getPoint();
+    }
+
+    @PostMapping("/pontos")
+    public String pontos(@RequestBody User user){
+        return Connection.find(user.getEmail()).toString();
+    }
+}
